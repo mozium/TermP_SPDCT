47c2206ad5508727f2acc0e4007a2ea23f9d4abe
mozium <ChengHsi.Yang@gmail.com>
2021-04-27 01:36:44 +0800

    finishing movement and preparing for creating animation

---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    private bool isMoving;
+    private Vector3 origPos, targetPos;
+    private float timeToMove = 0.25f;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow) && !isMoving)
+        {
+            StartCoroutine(movePlayer(Vector3.up));
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow) && !isMoving)
+        {
+            StartCoroutine(movePlayer(Vector3.down));
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) && !isMoving)
+        {
+            StartCoroutine(movePlayer(Vector3.right));
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) && !isMoving)
+        {
+            StartCoroutine(movePlayer(Vector3.left));
+        }
+    }
+
+    private IEnumerator movePlayer(Vector3 direction)
+    {
+        isMoving = true;
+
+        float elapsedTime = 0f;
+
+        origPos = transform.position;
+        targetPos = origPos + direction;
+
+        while (elapsedTime < timeToMove)
+        {
+            transform.position = Vector3.Lerp(origPos, targetPos, (elapsedTime / timeToMove));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = targetPos;
+
+        isMoving = false;
+    }
+}
